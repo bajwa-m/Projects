eeff900193b5f30a477aee748c5072dae5d6805e
Moneeb Irshad Bajwa <bajwa.m@husky.neu.edu>
2018-09-20 11:08:05 -0400

    Update findHydSeq.py

--- findHydSeq.py
+++ findHydSeq.py
@@ -15,3 +15,14 @@ with open("test.fasta") as file:
                 if header_count==1:
                     print("\n", record.group(1),"\n") 
                 print(h.group(), "found at position ", str(pos))
+"""Output:
+ >P30450 | Homo sapiens (Human). | NCBI_TaxID=9606; | 365 |    Name=HLA-A; Synonyms=HLAA; 
+
+AVVAAVMW found at position  325
+
+ >A7MBM2 | Homo sapiens (Human). | NCBI_TaxID=9606; | 1401 |    Name=DISP2; Synonyms=DISPB, KIAA1742; 
+
+VAVLMLCLAVIFLC found at position  170
+LLALVAIFF found at position  493
+IWICWFAALAA found at position  705
+LALALAFA found at position  970"""
